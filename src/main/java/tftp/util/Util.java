895f6fb68ca12fb0a08847ad6c80b2fad5d5b2f1
avigh <avightclav@gmail.com>
2020-05-05 19:07:03 +0300

    Unused method deleted

--- src/main/java/tftp/util/Util.java
+++ src/main/java/tftp/util/Util.java
@@ -1,20 +1,6 @@
 package tftp.util;
 
-import java.util.Random;
-
 public class Util {
-
-    public static String randomFilename(int filenameLength) {
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        Random random = new Random();
-
-        return random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(filenameLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-    }
 
     public static byte[] appendNullByte(byte[] data) {
         byte[] extendedSizeData = new byte[data.length + 1];
@@ -22,7 +8,6 @@ public class Util {
         extendedSizeData[extendedSizeData.length - 1] = (byte) 0;
         return extendedSizeData;
     }
-    // https://www.baeldung.com/java-random-string
 
     public static byte[] shortToBytesArray(short number) {
         return new byte[]{(byte) (number >>> 8), (byte) (number & 0xFF)};
